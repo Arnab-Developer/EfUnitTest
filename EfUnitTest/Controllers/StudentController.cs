d748cf5887272bcfc5a6e6efe7e4241be0ae70be
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-03-27 12:18:39 +0530

    Add project files.

---
+++ EfUnitTest/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using EfUnitTest.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace EfUnitTest.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _studentContext;
+
+        public StudentController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+
+        public IActionResult Index()
+        {
+            var students = _studentContext.Students
+                .OrderBy(student => student.Id)
+                .ToList();
+            return View(students);
+        }
+    }
+}
